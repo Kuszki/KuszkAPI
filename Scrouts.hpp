6133c4e0e40bbb6d57caf23e29b199e8cc794033
Łukasz Dróżdż <l.drozdz@o2.pl>
2014-07-29 17:38:30 +0200

    update docs

--- Scrouts.hpp
+++ Scrouts.hpp
@@ -12,14 +12,14 @@ Aby nie dołączać skróconych nazw (na przykład gdy kolidują z użytymi w in
 /// Jeśli nie chcesz używać skróconych nazw, bądź kolidują one z nazwami w Twoim projekcie, zwyczajnie zdefiniuj to makro przed dołączeniem biblioteki.
 #define KUSZKAPI_NOSCROUTS
 
-#define SRV KuszkAPI::Sockets::Server //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Server.
-#define CLI KuszkAPI::Sockets::Client //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Client.
+#define SRV KuszkAPI::Sockets::Server        //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Server.
+#define CLI KuszkAPI::Sockets::Client        //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Client.
 
-#define STR KuszkAPI::Containers::String //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::String.
+#define STR KuszkAPI::Containers::String     //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::String.
 
-#define CON KuszkAPI::Core::Console //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Console.
+#define CON KuszkAPI::Core::Console          //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Console.
 
-#define WND KuszkAPI::Forms::Form //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Form.
+#define WND KuszkAPI::Forms::Form            //!< Skrócona nazwa dla klasy KuszkAPI::Sockets::Form.
 
 #undef KUSZKAPI_NOSCROUTS
 // ------------------------------
