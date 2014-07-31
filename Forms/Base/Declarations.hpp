1850a8b76e0bb9d35373a9de3a95d0b431cea7c4
Łukasz Dróżdż <l.drozdz@o2.pl>
2014-07-31 11:31:39 +0200

    update docs

--- Forms/Base/Declarations.hpp
+++ Forms/Base/Declarations.hpp
@@ -14,163 +14,225 @@ class DateControl;
 // ---- WINDOW DECLARATIONS -----
 class Window
 {
-      protected:
-              class Timer
-              {
-              private:
-                       const HWND& hOwner;
-                       Containers::Map<UINT_PTR, unsigned> mTimer;
-              public:
-                       Timer(const HWND& hOwn);
-                       ~Timer(void);
-                       UINT_PTR Add(unsigned uTick,
-                                    unsigned uId,
-                                    TIMERPROC fProc = NULL);
-                       void Delete(unsigned uId);
-                       UINT_PTR GetHandle(unsigned uNumer) const;
-                       void Clean(void);
-                       UINT_PTR operator[] (unsigned uNumer) const;
-              };
-              const HWND& hOwner;
-              HWND hUchwyt;
-      public:
-              struct TCord
-              {
-                       unsigned X;
-                       unsigned Y;
-              };
-              Timer Timer;
-              Window(const HWND& hOwn);
-              virtual ~Window(void);
-              void Show(int iStyl = SW_SHOWDEFAULT);
-              bool Visible(void) const;
-              bool Exists(void) const;
-              void Focus(void);
-              void Ground(void);
-              void Enable(void);
-              void Disable(void);
-              LRESULT Msg(UINT mMessage,
-                          LPARAM lParam = 0,
-                          WPARAM wParam = 0);
-              const HWND& GetHandle(void) const;
-              void SetPos(int iXPos = 0,
-                          int iYPos = 0);
-              void SetPos(const TCord& tPos);
-              Window::TCord GetPos(void) const;
-              void SetSize(int iSzerokosc,
-                           int iWysokosc);
-              Window::TCord GetSize(void) const;
-              void SetRegion(HRGN hRegion);
-              void SetTopMost(bool bTop);
-              void SetWndLong(int iId,
-                              unsigned uWartosc);
-              unsigned GetWndlong(int iId) const;
-              void SetClaLong(int iId,
-                              unsigned uWartosc);
-              unsigned GetClaLong(int iId) const;
-              operator const HWND& (void) const;
-              virtual void Destroy(void) = 0;
-              virtual void Clean(void) = 0;
-              static void SetWndName(HWND hWnd,
-                                     const Containers::String& sName);
-              static Containers::String GetWndName(HWND hWnd);
+
+     protected:
+
+          class Timer
+          {
+               private:
+
+                    const HWND& hOwner;
+
+                    Containers::Map<UINT_PTR, unsigned> mTimer;
+
+               public:
+
+                    Timer(const HWND& hOwn);
+                    ~Timer(void);
+
+                    UINT_PTR Add(unsigned uTick,
+                                 unsigned uId,
+                                 TIMERPROC fProc = NULL);
+
+                    void Delete(unsigned uId);
+                    UINT_PTR GetHandle(unsigned uNumer) const;
+                    void Clean(void);
+
+                    UINT_PTR operator[] (unsigned uNumer) const;
+
+          };
+
+          const HWND& hOwner;
+          HWND hUchwyt;
+
+     public:
+
+          struct Cord
+          {
+
+               unsigned X;
+               unsigned Y;
+
+          };
+
+          Timer Watch;
+
+          Window(const HWND& hOwn);
+          virtual ~Window(void);
+
+          void Show(int iStyl = SW_SHOWDEFAULT);
+          bool Visible(void) const;
+          bool Exists(void) const;
+          void Focus(void);
+          void Ground(void);
+          void Enable(void);
+          void Disable(void);
+          LRESULT Msg(UINT mMessage,
+                      LPARAM lParam = 0,
+                      WPARAM wParam = 0);
+          const HWND& GetHandle(void) const;
+          void SetPos(int iXPos = 0,
+                      int iYPos = 0);
+          void SetPos(const Cord& tPos);
+          Window::Cord GetPos(void) const;
+          void SetSize(int iSzerokosc,
+                       int iWysokosc);
+          Window::Cord GetSize(void) const;
+          void SetRegion(HRGN hRegion);
+          void SetTopMost(bool bTop);
+          void SetWndLong(int iId,
+                          unsigned uWartosc);
+          unsigned GetWndlong(int iId) const;
+          void SetClaLong(int iId,
+                          unsigned uWartosc);
+          unsigned GetClaLong(int iId) const;
+
+          operator const HWND& (void) const;
+
+          virtual void Destroy(void) = 0;
+          virtual void Clean(void) = 0;
+
+          static void SetWndName(HWND hWnd,
+                                 const Containers::String& sName);
+          static Containers::String GetWndName(HWND hWnd);
+
 };
 // ------------------------------
 
 // ---- CONTROL DECLARATIONS ----
 class Control : public Window
 {
-      protected:
-              class ToopTip
-              {
-              private:
-                       const HWND& hWindow;
-                       const HWND& hOwner;
-                       TOOLINFO tToolTipInfo;
-                       HWND hUchwyt;
-              public:
-                       ToopTip(const HWND& hWnd,
-                                const HWND& hOwn);
-                       ~ToopTip(void);
-                       void Add(const Containers::String& sTekst,
-                                const Containers::String& sTytul = Containers::String(),
-                                unsigned uIkona = 0);
-                       void Delete(void);
-                       void Set(const Containers::String& sTekst = Containers::String(),
-                                const Containers::String& sTytul = Containers::String(),
-                                unsigned uIkona = 0);
-              };
-              static HINSTANCE hInstance;
-              static HFONT hDefFont;
-              WNDPROC fDefProc;
-              unsigned uId;
-      public:
-              ToopTip Tooltip;
-              Control(const HWND& hOwn,
-                       unsigned uCtrId);
-              virtual ~Control(void);
-              void SetFont(HFONT hCzcionka = NULL);
-              HWND GetOwner(void) const;
-              unsigned GetId(void) const;
-              WNDPROC GetDefProc(void) const;
-              void Subclass(WNDPROC fProc);
-              void Destroy(void);
-              void Clean(void);
-              operator WNDPROC (void) const;
+
+     protected:
+
+          class ToopTip
+          {
+
+               private:
+
+                    const HWND& hWindow;
+                    const HWND& hOwner;
+
+                    TOOLINFO tToolTipInfo;
+
+                    HWND hUchwyt;
+
+               public:
+
+                    ToopTip(const HWND& hWnd,
+                            const HWND& hOwn);
+                    ~ToopTip(void);
+
+                    void Add(const Containers::String& sTekst,
+                             const Containers::String& sTytul = Containers::String(),
+                             unsigned uIkona = 0);
+                    void Delete(void);
+                    void Set(const Containers::String& sTekst = Containers::String(),
+                             const Containers::String& sTytul = Containers::String(),
+                             unsigned uIkona = 0);
+
+          };
+
+          static HINSTANCE hInstance;
+          static HFONT hDefFont;
+
+          WNDPROC fDefProc;
+
+          unsigned uId;
+
+     public:
+
+          ToopTip Tooltip;
+          Control(const HWND& hOwn,
+                  unsigned uCtrId);
+          virtual ~Control(void);
+
+          void SetFont(HFONT hCzcionka = NULL);
+          HWND GetOwner(void) const;
+          unsigned GetId(void) const;
+          WNDPROC GetDefProc(void) const;
+          void Subclass(WNDPROC fProc);
+          void Destroy(void);
+          void Clean(void);
+
+          operator WNDPROC (void) const;
+
 };
 // ------------------------------
 
 // ---- TEXTCTR DECLARATIONS ----
 class TextControl : public Control
 {
-      public:
-              TextControl(const HWND& hOwn,
-                           unsigned uCtrId);
-              void SetText(const Containers::String& sText);
-              Containers::String GetText(void) const;
-              operator Containers::String (void) const;
+
+     public:
+
+          TextControl(const HWND& hOwn,
+                      unsigned uCtrId);
+
+          void SetText(const Containers::String& sText);
+          Containers::String GetText(void) const;
+
+          operator Containers::String (void) const;
+
 };
 // ------------------------------
 
 // --- CHECKCTR DECLARATIONS ----
 class CheckBoxControl : public TextControl
 {
-      public:
-              CheckBoxControl(const HWND& hOwn,
-                            unsigned uCtrId);
-              bool GetCheck(void) const;
-              operator bool (void) const;
-              virtual void SetCheckBox(bool bZaznacz = true) = 0;
+
+     public:
+
+          CheckBoxControl(const HWND& hOwn,
+                          unsigned uCtrId);
+
+          bool GetCheck(void) const;
+
+          operator bool (void) const;
+
+          virtual void SetCheckBox(bool bZaznacz = true) = 0;
+
 };
 // ------------------------------
 
 // ---- LISTCTR DECLARATIONS ----
 class ListControl : public Control
 {
-      public:
-              ListControl(const HWND& hOwn,
-                           unsigned uCtrId);
-              bool Empty(void) const;
-              operator Containers::Strings (void) const;
-              operator bool (void) const;
-              virtual void DeleteItem(unsigned uNumer = 0) = 0;
-              virtual Containers::String GetItem(unsigned uNumer = 0) const = 0;
-              virtual Containers::Strings GetItems(void) const = 0;
-              virtual unsigned GetIndex(void) const = 0;
-              virtual unsigned Capacity(void) const = 0;
-              virtual void CleanItems(void) = 0;
+
+     public:
+
+          ListControl(const HWND& hOwn,
+                      unsigned uCtrId);
+
+          bool Empty(void) const;
+
+          operator Containers::Strings (void) const;
+          operator bool (void) const;
+
+          virtual void DeleteItem(unsigned uNumer = 0) = 0;
+          virtual Containers::String GetItem(unsigned uNumer = 0) const = 0;
+          virtual Containers::Strings GetItems(void) const = 0;
+          virtual unsigned GetIndex(void) const = 0;
+          virtual unsigned Capacity(void) const = 0;
+          virtual void CleanItems(void) = 0;
+
 };
 // ------------------------------
 
 // ---- DATECTR DECLARATIONS ----
 class DateControl : public Control
 {
-      public:
-              DateControl(const HWND& hOwn,
-                          unsigned uCtrId);
-              virtual void SetDate(const DateTime::Date& dDate) = 0;
-              virtual DateTime::Date GetDate(void) const = 0;
-              operator DateTime::Date(void) const;
+
+     public:
+
+          DateControl(const HWND& hOwn,
+                      unsigned uCtrId);
+
+          operator DateTime::Date(void) const;
+
+          virtual void SetDate(const DateTime::Date& dDate) = 0;
+          virtual DateTime::Date GetDate(void) const = 0;
+
 };
 // ------------------------------
 
--- Forms/Base/Window.cpp
+++ Forms/Base/Window.cpp
@@ -4,180 +4,199 @@ KuszkAPI::Forms::Window::Timer::Timer(const HWND& hOwn) : hOwner(hOwn) {}
 
 KuszkAPI::Forms::Window::Timer::~Timer(void)
 {
-      Clean();
+     Clean();
 }
 
 UINT_PTR KuszkAPI::Forms::Window::Timer::Add(unsigned uTick, unsigned uId, TIMERPROC fProc)
 {
-      UINT_PTR uTmp = SetTimer(hOwner, uId, uTick, fProc);
-      if (uTmp) if (mTimer.Add(uTmp, uId)) return uTmp; else KillTimer(hOwner, uId);
-      return 0;
+     UINT_PTR uTmp = SetTimer(hOwner, uId, uTick, fProc);
+
+     if (uTmp) if (mTimer.Add(uTmp, uId)) return uTmp; else KillTimer(hOwner, uId);
+
+     return 0;
 }
 
 void KuszkAPI::Forms::Window::Timer::Delete(unsigned uNumer)
 {
-      KillTimer(hOwner, uNumer);
-      mTimer.Delete(uNumer);
+     KillTimer(hOwner, uNumer);
+
+     mTimer.Delete(uNumer);
 }
 
 UINT_PTR KuszkAPI::Forms::Window::Timer::GetHandle(unsigned uNumer) const
 {
-      return mTimer.GetData(uNumer);
+     return mTimer.GetData(uNumer);
 }
 
 void KuszkAPI::Forms::Window::Timer::Clean(void)
 {
-      for (int i = 1; i <= mTimer.Capacity(); i++) KillTimer(hOwner, mTimer.GetKey(i));
-      mTimer.Clean();
+     for (int i = 1; i <= mTimer.Capacity(); i++) KillTimer(hOwner, mTimer.GetKey(i));
+
+     mTimer.Clean();
 }
 
 UINT_PTR KuszkAPI::Forms::Window::Timer::operator[] (unsigned uNumer) const
 {
-      return mTimer.GetData(uNumer);
+     return mTimer.GetData(uNumer);
 }
 
 KuszkAPI::Forms::Window::Window(const HWND& hOwn)
-: hOwner(hOwn), Timer(hUchwyt)
+: hOwner(hOwn), Watch(hUchwyt)
 {
-      hUchwyt = NULL;
+     hUchwyt = NULL;
 }
 
 KuszkAPI::Forms::Window::~Window(void)
 {
-      hUchwyt = NULL;
+     hUchwyt = NULL;
 }
 
 void KuszkAPI::Forms::Window::Show(int iStyl)
 {
-      ShowWindow(hUchwyt, iStyl);
+     ShowWindow(hUchwyt, iStyl);
 }
 
 bool KuszkAPI::Forms::Window::Visible(void) const
 {
-      return IsWindowVisible(hUchwyt);
+     return IsWindowVisible(hUchwyt);
 }
 
 bool KuszkAPI::Forms::Window::Exists(void) const
 {
-      return hUchwyt;
+     return hUchwyt;
 }
 
 void KuszkAPI::Forms::Window::Focus(void)
 {
-      SetFocus(hUchwyt);
+     SetFocus(hUchwyt);
 }
 
 void KuszkAPI::Forms::Window::Ground(void)
 {
-      SetForegroundWindow(hUchwyt);
+     SetForegroundWindow(hUchwyt);
 }
 
 void KuszkAPI::Forms::Window::Enable(void)
 {
-      EnableWindow(hUchwyt, true);
+     EnableWindow(hUchwyt, true);
 }
 
 void KuszkAPI::Forms::Window::Disable(void)
 {
-      EnableWindow(hUchwyt, false);
+     EnableWindow(hUchwyt, false);
 }
 
 LRESULT KuszkAPI::Forms::Window::Msg(UINT mMessage, LPARAM lParam, WPARAM wParam)
 {
-      return SendMessage(hUchwyt, mMessage, lParam, wParam);
+     return SendMessage(hUchwyt, mMessage, lParam, wParam);
 }
 
 const HWND& KuszkAPI::Forms::Window::GetHandle(void) const
 {
-      return hUchwyt;
+     return hUchwyt;
 }
 
 void KuszkAPI::Forms::Window::SetPos(int iXPos, int iYPos)
 {
-      RECT rObszar;
-      GetClientRect(hUchwyt, &rObszar);
-      if (!iXPos) iXPos = GetSystemMetrics(SM_CXFULLSCREEN) / 2 - (rObszar.right - rObszar.left) / 2;
-      if (!iYPos) iYPos = GetSystemMetrics(SM_CYFULLSCREEN) / 2 - (rObszar.bottom - rObszar.top) / 2;
-      SetWindowPos(hUchwyt, hOwner, iXPos, iYPos, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+     RECT rObszar;
+
+     GetClientRect(hUchwyt, &rObszar);
+
+     if (!iXPos) iXPos = GetSystemMetrics(SM_CXFULLSCREEN) / 2 - (rObszar.right - rObszar.left) / 2;
+     if (!iYPos) iYPos = GetSystemMetrics(SM_CYFULLSCREEN) / 2 - (rObszar.bottom - rObszar.top) / 2;
+
+     SetWindowPos(hUchwyt, hOwner, iXPos, iYPos, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 }
 
-void KuszkAPI::Forms::Window::SetPos(const TCord& tPos)
+void KuszkAPI::Forms::Window::SetPos(const Cord& tPos)
 {
-      SetPos(tPos.X, tPos.Y);
+     SetPos(tPos.X, tPos.Y);
 }
 
-KuszkAPI::Forms::Window::TCord KuszkAPI::Forms::Window::GetPos(void) const
+KuszkAPI::Forms::Window::Cord KuszkAPI::Forms::Window::GetPos(void) const
 {
-      TCord tCord;
-      POINT pTmp;
-      memset(&pTmp, 0, sizeof(POINT));
-      MapWindowPoints(hUchwyt, hOwner, &pTmp, 1);
-      tCord.X = pTmp.x;
-      tCord.Y = pTmp.y;
-      return tCord;
+     Cord tCord;
+     POINT pTmp;
+
+     memset(&pTmp, 0, sizeof(POINT));
+
+     MapWindowPoints(hUchwyt, hOwner, &pTmp, 1);
+
+     tCord.X = pTmp.x;
+     tCord.Y = pTmp.y;
+
+     return tCord;
 }
 
 void KuszkAPI::Forms::Window::SetSize(int iSzerokosc, int iWysokosc)
 {
-      SetWindowPos(hUchwyt, hOwner, 0, 0, iSzerokosc, iWysokosc, SWP_NOMOVE | SWP_NOZORDER);
-      UpdateWindow(hOwner);
+     SetWindowPos(hUchwyt, hOwner, 0, 0, iSzerokosc, iWysokosc, SWP_NOMOVE | SWP_NOZORDER);
+
+     UpdateWindow(hOwner);
 }
 
-KuszkAPI::Forms::Window::TCord KuszkAPI::Forms::Window::GetSize(void) const
+KuszkAPI::Forms::Window::Cord KuszkAPI::Forms::Window::GetSize(void) const
 {
-      TCord tTmp;
-      RECT rTmp;
-      GetWindowRect(hUchwyt, &rTmp);
-      tTmp.X = rTmp.right - rTmp.left;
-      tTmp.Y = rTmp.bottom - rTmp.top;
-      return tTmp;
+     Cord tTmp;
+     RECT rTmp;
+
+     GetWindowRect(hUchwyt, &rTmp);
+
+     tTmp.X = rTmp.right - rTmp.left;
+     tTmp.Y = rTmp.bottom - rTmp.top;
+
+     return tTmp;
 }
 
 void KuszkAPI::Forms::Window::SetRegion(HRGN hRegion)
 {
-      SetWindowRgn(hUchwyt, hRegion, true);
+     SetWindowRgn(hUchwyt, hRegion, true);
 }
 
 void KuszkAPI::Forms::Window::SetTopMost(bool bTop)
 {
-      SetWindowPos(hUchwyt, bTop ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+     SetWindowPos(hUchwyt, bTop ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 }
 
 void KuszkAPI::Forms::Window::SetWndLong(int iId, unsigned uWartosc)
 {
-      SetWindowLong(hUchwyt, iId, uWartosc);
+     SetWindowLong(hUchwyt, iId, uWartosc);
 }
 
 unsigned KuszkAPI::Forms::Window::GetWndlong(int iId) const
 {
-      return GetWindowLong(hUchwyt, iId);
+     return GetWindowLong(hUchwyt, iId);
 }
 
 void KuszkAPI::Forms::Window::SetClaLong(int iId, unsigned uWartosc)
 {
-      SetClassLong(hUchwyt, iId, uWartosc);
+     SetClassLong(hUchwyt, iId, uWartosc);
 }
 
 unsigned KuszkAPI::Forms::Window::GetClaLong(int iId) const
 {
-      return GetClassLong(hUchwyt, iId);
+     return GetClassLong(hUchwyt, iId);
 }
 
 KuszkAPI::Forms::Window::operator const HWND& (void) const
 {
-      return hUchwyt;
+     return hUchwyt;
 }
 
 void KuszkAPI::Forms::Window::SetWndName(HWND hWnd, const Containers::String& sName)
 {
-      SetWindowText(hWnd, sName.Str());
+     SetWindowText(hWnd, sName.Str());
 }
 
 Containers::String KuszkAPI::Forms::Window::GetWndName(HWND hWnd)
 {
-      unsigned uTmp = GetWindowTextLength(hWnd) + 1;
-      TCHAR cBufor[uTmp + 1];
-      GetWindowText(hWnd, cBufor, uTmp);
-      cBufor[uTmp] = 0;
-      return cBufor;
+     unsigned uTmp = GetWindowTextLength(hWnd) + 1;
+
+     TCHAR cBufor[uTmp + 1];
+
+     GetWindowText(hWnd, cBufor, uTmp);
+
+     cBufor[uTmp] = 0;
+
+     return cBufor;
 }
