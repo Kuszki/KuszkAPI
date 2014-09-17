8599b7b82c8cede51e3364bb97c3382e3700bcb2
Łukasz Dróżdż <l.drozdz@o2.pl>
2014-09-17 22:16:01 +0200

    Add new functions to Core::Ini

--- Core/Data/Declarations.hpp
+++ Core/Data/Declarations.hpp
@@ -10,50 +10,67 @@ class Reg;
 // ------ INI DECLARATIONS ------
 class Ini
 {
-      private:
-              Containers::String sFile;
-      public:
-              Ini(const Containers::String& sFileName = Containers::String());
-              ~Ini(void);
-              void SetFile(const Containers::String& sFileName);
-              Containers::String GetFile(void) const;
-              bool SetStr(const Containers::String& sSection,
-                             const Containers::String& sName,
-                             const Containers::String& sValue);
-              Containers::String GetStr(const Containers::String& sSection,
-                               const Containers::String& sName) const;
-              bool SetInt(const Containers::String& sSection,
-                          const Containers::String& sName,
-                          int iValue);
-              int GetInt(const Containers::String& sSection,
-                         const Containers::String& sName) const;
-              void Clean(void);
+
+     protected:
+
+          Containers::String sFile;
+
+     public:
+
+          Ini(const Containers::String& sFileName = Containers::String());
+          ~Ini(void);
+
+          void SetFile(const Containers::String& sFileName);
+          Containers::String GetFile(void) const;
+          bool SetStr(const Containers::String& sSection,
+                      const Containers::String& sName,
+                      const Containers::String& sValue);
+          Containers::String GetStr(const Containers::String& sSection,
+                                    const Containers::String& sName) const;
+          bool SetInt(const Containers::String& sSection,
+                      const Containers::String& sName,
+                      int iValue);
+          int GetInt(const Containers::String& sSection,
+                     const Containers::String& sName) const;
+          Containers::Strings GetSections() const;
+          Containers::Strings GetKeys(const Containers::String& sSection) const;
+          Containers::Map<Containers::String, Containers::String> GetStrValues(const Containers::String& sSection) const;
+          Containers::Map<int, Containers::String> GetIntValues(const Containers::String& sSection) const;
+          void Clean(void);
+
 };
 // ------------------------------
 
 // ------ REG DECLARATIONS ------
 class Reg
 {
-      private:
-              Containers::Vector<HKEY> vBack;
-              HKEY hKey;
-      public:
-              Reg(HKEY hStart = NULL);
-              ~Reg(void);
-              bool Open(const Containers::String& sName);
-              void Open(HKEY hStart);
-              bool Back(void);
-              bool AddKey(const Containers::String& sName,
-                          bool bOpen = true);
-              bool DeleteKey(const Containers::String& sName);
-              bool SetStr(const Containers::String& sName,
-                          const Containers::String& sValue);
-              Containers::String GetStr(const Containers::String& sName);
-              bool SetInt(const Containers::String& sName,
-                          unsigned uValue);
-              int GetInt(const Containers::String& sName);
-              bool DeleteItem(const Containers::String& sName);
-              void Clean(void);
+
+     protected:
+
+          Containers::Vector<HKEY> vBack;
+
+          HKEY hKey;
+
+     public:
+
+          Reg(HKEY hStart = NULL);
+          ~Reg(void);
+
+          bool Open(const Containers::String& sName);
+          void Open(HKEY hStart);
+          bool Back(void);
+          bool AddKey(const Containers::String& sName,
+                      bool bOpen = true);
+          bool DeleteKey(const Containers::String& sName);
+          bool SetStr(const Containers::String& sName,
+                      const Containers::String& sValue);
+          Containers::String GetStr(const Containers::String& sName);
+          bool SetInt(const Containers::String& sName,
+                      unsigned uValue);
+          int GetInt(const Containers::String& sName);
+          bool DeleteItem(const Containers::String& sName);
+          void Clean(void);
+
 };
 // ------------------------------
 
--- Core/Data/Ini.cpp
+++ Core/Data/Ini.cpp
@@ -2,60 +2,126 @@ using namespace KuszkAPI;
 
 KuszkAPI::Core::Ini::Ini(const Containers::String& sFileName)
 {
-      if (sFileName) SetFile(sFileName);
+     if (sFileName) SetFile(sFileName);
 }
 
 KuszkAPI::Core::Ini::~Ini(void)
 {
-      sFile.Clean();
+     sFile.Clean();
 }
 
 void KuszkAPI::Core::Ini::SetFile(const Containers::String& sFileName)
 {
-      if (sFileName.Contain(TEXT(':'))) sFile = sFileName; else{
-              TCHAR* pcBufor = new TCHAR[MAX_PATH];
-              unsigned uTmp = GetModuleFileName(NULL, pcBufor, MAX_PATH) - 1;
-              for (bool bNext = true; uTmp >= 0 && bNext; uTmp--) if (pcBufor[uTmp] == TEXT('\\')){
-                       pcBufor[++uTmp] = 0;
-                       bNext = false;
-              }
-              sFile = Containers::String(pcBufor) + sFileName;
-      }
+     if (sFileName.Contain(TEXT(':'))) sFile = sFileName; else{
+
+          TCHAR* pcBufor = new TCHAR[MAX_PATH];
+
+          unsigned uTmp = GetModuleFileName(NULL, pcBufor, MAX_PATH) - 1;
+
+          for (bool bNext = true; uTmp >= 0 && bNext; uTmp--) if (pcBufor[uTmp] == TEXT('\\')){
+
+               pcBufor[++uTmp] = 0;
+
+               bNext = false;
+
+          }
+
+          sFile = Containers::String(pcBufor) + sFileName;
+
+     }
 }
 
 Containers::String KuszkAPI::Core::Ini::GetFile(void) const
 {
-      return sFile;
+     return sFile;
 }
 
 bool KuszkAPI::Core::Ini::SetStr(const Containers::String& sSection, const Containers::String& sName, const Containers::String& sValue)
 {
-      if (sFile) return WritePrivateProfileString(sSection.Str(), sName.Str(), sValue.Str(), sFile.Str()); else return false;
+     if (sFile) return WritePrivateProfileString(sSection.Str(), sName.Str(), sValue.Str(), sFile.Str());
+     else return false;
 }
 
 Containers::String KuszkAPI::Core::Ini::GetStr(const Containers::String& sSection, const Containers::String& sName) const
 {
-      Containers::String sBufor;
-      if (!sFile) return sBufor;
-      TCHAR* pcBufor = new TCHAR[MAX_PATH];
-      unsigned uTmp = GetPrivateProfileString(sSection.Str(), sName.Str(), NULL, pcBufor, MAX_PATH, sFile.Str());
-      pcBufor[uTmp] = 0;
-      sBufor = pcBufor;
-      delete [] pcBufor;
-      return sBufor;
+     Containers::String sBufor;
+
+     if (!sFile) return sBufor;
+
+     TCHAR* pcBufor = new TCHAR[MAX_PATH];
+
+     unsigned uTmp = GetPrivateProfileString(sSection.Str(), sName.Str(), NULL, pcBufor, MAX_PATH, sFile.Str());
+
+     pcBufor[uTmp] = 0;
+
+     sBufor = pcBufor;
+
+     delete [] pcBufor;
+
+     return sBufor;
 }
 
 bool KuszkAPI::Core::Ini::SetInt(const Containers::String& sSection, const Containers::String& sName, int iValue)
 {
-      if (sFile) return WritePrivateProfileString(sSection.Str(), sName.Str(), Containers::String(iValue).Str(), sFile.Str()); else return false;
+     if (sFile) return WritePrivateProfileString(sSection.Str(), sName.Str(), Containers::String(iValue).Str(), sFile.Str());
+     else return false;
 }
 
 int KuszkAPI::Core::Ini::GetInt(const Containers::String& sSection, const Containers::String& sName) const
 {
-      if (sFile) return GetPrivateProfileInt(sSection.Str(), sName.Str(), 0, sFile.Str()); else return 0;
+     if (sFile) return GetPrivateProfileInt(sSection.Str(), sName.Str(), 0, sFile.Str());
+     else return 0;
+}
+
+Containers::Strings KuszkAPI::Core::Ini::GetSections() const
+{
+     TCHAR* pcBufor = new TCHAR[MAX_PATH];
+
+     unsigned uTmp = GetPrivateProfileString(NULL, NULL, NULL, pcBufor, MAX_PATH, sFile.Str());
+
+     Containers::Vector<char>::Change(pcBufor, uTmp, 0, 10, true);
+
+     pcBufor[uTmp - 1] = 0;
+
+	return Containers::Strings(pcBufor);
+}
+
+Containers::Strings KuszkAPI::Core::Ini::GetKeys(const Containers::String& sSection) const
+{
+	TCHAR* pcBufor = new TCHAR[MAX_PATH];
+
+     unsigned uTmp = GetPrivateProfileString(sSection.Str(), NULL, NULL, pcBufor, MAX_PATH, sFile.Str());
+
+     Containers::Vector<char>::Change(pcBufor, uTmp, 0, 10, true);
+
+     pcBufor[uTmp - 1] = 0;
+
+	return Containers::Strings(pcBufor);
+}
+
+Containers::Map<Containers::String, Containers::String> KuszkAPI::Core::Ini::GetStrValues(const Containers::String& sSection) const
+{
+	Containers::Strings sKeys = GetKeys(sSection);
+
+	Containers::Map<Containers::String, Containers::String> mValues;
+
+	for (int i = 1; i <= sKeys.Capacity(); i++) mValues.Add(GetStr(sSection, sKeys[i]), sKeys[i]);
+
+	return mValues;
+}
+
+Containers::Map<int, Containers::String> KuszkAPI::Core::Ini::GetIntValues(const Containers::String& sSection) const
+{
+	Containers::Strings sKeys = GetKeys(sSection);
+
+	Containers::Map<int, Containers::String> mValues;
+
+	for (int i = 1; i <= sKeys.Capacity(); i++) mValues.Add(GetInt(sSection, sKeys[i]), sKeys[i]);
+
+	return mValues;
 }
 
 void KuszkAPI::Core::Ini::Clean(void)
 {
-      sFile.Clean();
+     sFile.Clean();
 }
