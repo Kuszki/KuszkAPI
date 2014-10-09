dd3d792d1cb81cc4582ec29c9df5e83ce74db896
Łukasz Dróżdż <l.drozdz@o2.pl>
2014-10-09 22:13:09 +0200

    Update Layout

--- Containers/Files/Declarations.hpp
+++ Containers/Files/Declarations.hpp
@@ -10,32 +10,41 @@ template<typename tnData, typename tnHead> class File;
 template<typename tnData, typename tnHead = int>
 class File
 {
-      private:
-              String sNazwa;
-              HANDLE hUchwyt;
-              unsigned uDataSize;
-              unsigned uHeadSize;
-              unsigned uMode;
-      public:
-              Vector<tnData> Data;
-              tnHead Head;
-              File(const String& sNazwaPliku,
-                    unsigned uFileMode = MODE_STREAM,
-                    unsigned uCreate = OPEN_ALWAYS,
-                    unsigned uAccess = GENERIC_READ | GENERIC_WRITE,
-                    unsigned uShare = FILE_SHARE_READ);
-              File(void);
-              ~File(void);
-              bool Assign(const String& sNazwaPliku,
-                          unsigned uCreate = OPEN_ALWAYS,
-                          unsigned uAccess = GENERIC_READ | GENERIC_WRITE,
-                          unsigned uShare = FILE_SHARE_READ);
-              bool Load(void);
-              bool Save(void) const;
-              bool Copy(const String& sNazwaPliku) const;
-              void SetMode(unsigned uNewMode);
-              unsigned GetMode(void) const;
-              void Clean(void);
+     private:
+
+          String sNazwa;
+
+          HANDLE hUchwyt;
+
+          unsigned uDataSize;
+          unsigned uHeadSize;
+          unsigned uMode;
+
+     public:
+
+          Vector<tnData> Data;
+
+          tnHead Head;
+
+          File(const String& sNazwaPliku,
+               unsigned uFileMode = MODE_STREAM,
+               unsigned uCreate = OPEN_ALWAYS,
+               unsigned uAccess = GENERIC_READ | GENERIC_WRITE,
+               unsigned uShare = FILE_SHARE_READ);
+          File(void);
+          ~File(void);
+
+          bool Assign(const String& sNazwaPliku,
+                      unsigned uCreate = OPEN_ALWAYS,
+                      unsigned uAccess = GENERIC_READ | GENERIC_WRITE,
+                      unsigned uShare = FILE_SHARE_READ);
+          bool Load(void);
+          bool Save(void) const;
+          bool Copy(const String& sNazwaPliku) const;
+          void SetMode(unsigned uNewMode);
+          unsigned GetMode(void) const;
+          void Clean(void);
+
 };
 // ------------------------------
 
