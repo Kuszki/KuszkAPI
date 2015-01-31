aa3e0b7b2760ec4088b8ac14e74b56ec1f30c147
Łukasz Dróżdż <l.drozdz@o2.pl>
2015-01-31 21:17:05 +0100

    Code layout update

--- Containers/Base/Container.cpp
+++ Containers/Base/Container.cpp
@@ -2,53 +2,53 @@ using namespace KuszkAPI;
 
 KuszkAPI::Containers::Container::Container(void)
 {
-      uIlosc = 0;
-      uMode = MODE_QUEUE;
+	uIlosc = 0;
+	uMode = MODE_QUEUE;
 }
 
 KuszkAPI::Containers::Container::~Container(void)
 {
-      uIlosc = 0;
-      uMode = 0;
+	uIlosc = 0;
+	uMode = 0;
 }
 
 unsigned KuszkAPI::Containers::Container::Capacity(void) const
 {
-      return uIlosc;
+	return uIlosc;
 }
 
 bool KuszkAPI::Containers::Container::Empty(void) const
 {
-      return !(bool) uIlosc;
+	return !(bool) uIlosc;
 }
 
 void KuszkAPI::Containers::Container::SetMode(unsigned uNewMode)
 {
-      uMode = uNewMode;
+	uMode = uNewMode;
 }
 
 unsigned KuszkAPI::Containers::Container::GetMode(void) const
 {
-      return uMode;
+	return uMode;
 }
 
 KuszkAPI::Containers::Container::operator bool (void) const
 {
-      return !Empty();
+	return !Empty();
 }
 
 template<typename tnData>
 bool KuszkAPI::Containers::Container::Change(tnData& tOne, tnData& tTwo)
 {
-      if (&tOne == &tTwo) return false;
-      tnData tTmp = tOne;
-      tOne = tTwo;
-      tTwo = tTmp;
-      return true;
+	if (&tOne == &tTwo) return false;
+	tnData tTmp = tOne;
+	tOne = tTwo;
+	tTwo = tTmp;
+	return true;
 }
 
 template<typename tnData>
 void KuszkAPI::Containers::Container::Copy(const tnData& tSource, tnData& tDestonation)
 {
-      memcpy(&tDestonation, &tSource, sizeof(tnData));
+	memcpy(&tDestonation, &tSource, sizeof(tnData));
 }
